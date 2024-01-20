459a09e0afa5790ea4fa6ee46cf8f4d5d94cdfb1
Brendan Chen <me@bchen.dev>
2024-01-19 23:03:14 -0800

    remove placeholder Item data

--- PowerToThePeople/PowerToThePeopleApp.swift
+++ PowerToThePeople/PowerToThePeopleApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct PowerToThePeopleApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Recording.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
