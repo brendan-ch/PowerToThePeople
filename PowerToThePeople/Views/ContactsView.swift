9d2e89dcdb387daad0e20bfd702eca4e75feb1c3
Brendan Chen <me@bchen.dev>
2024-01-20 13:51:31 -0800

    add emergency contacts form

--- PowerToThePeople/Views/ContactsView.swift
+++ PowerToThePeople/Views/ContactsView.swift
@@ -10,11 +10,33 @@ import SwiftUI
 
 /// View for editing emergency contacts.
 struct ContactsView: View {
+    
+    
     var body: some View {
-        VStack {
-            Text("Add emergency contacts to notify when you activate recording.")
+        VStack(alignment: .leading, spacing: 16) {
+            Form {
+                Section(header: Text("Contact #1")) {
+                    TextField("Name", text: .constant(""))
+                    TextField("Phone number", text: .constant(""))
+                    TextField("Message", text: .constant(""), axis: .vertical)
+                        .lineLimit(4, reservesSpace: true)
+                }
+                
+                Section(header: Text("Contact #2")) {
+                    TextField("Name", text: .constant(""))
+                    TextField("Phone number", text: .constant(""))
+                    TextField("Message", text: .constant(""), axis: .vertical)
+                        .lineLimit(4, reservesSpace: true)
+                }
+                
+                Button {} label: {
+                    Text("Clear Emergency Contacts")
+                        .foregroundStyle(.red)
+                }
+            }
         }
         .navigationTitle("Emergency Contacts")
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
