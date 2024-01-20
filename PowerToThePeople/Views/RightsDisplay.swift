117dacec41cba04adbf202c0804ff57fc1d0f5bf
Brendan Chen <me@bchen.dev>
2024-01-19 23:16:36 -0800

    add state logic

--- PowerToThePeople/Views/RightsDisplay.swift
+++ PowerToThePeople/Views/RightsDisplay.swift
@@ -7,18 +7,26 @@
 
 import Foundation
 import SwiftUI
+import AVFoundation
 
 // TO-DO: write out the actual rights display data
 // This is just a placeholder for the camera stuff
 
 struct RightsDisplay: View {
+    @State private var recording: Bool = false
+    
     func toggleRecording() {
         print("Recording toggled")
+        recording = !recording
     }
     
     var body: some View {
         Button(action: toggleRecording) {
-            Text("Toggle Recording")
+            if recording {
+                Text("Stop Recording")
+            } else {
+                Text("Start Recording")
+            }
         }
     }
 }
