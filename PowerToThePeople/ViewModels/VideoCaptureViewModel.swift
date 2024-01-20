f174ee17b85b036140ce8578e532c1ac1e335647
Brendan Chen <me@bchen.dev>
2024-01-20 00:27:29 -0800

    add VideoCaptureVIewModel class

---
+++ PowerToThePeople/ViewModels/VideoCaptureViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  VideoCaptureViewModel.swift
+//  PowerToThePeople
+//
+//  Created by Brendan Chen on 2024.01.20.
+//
+
+import Foundation
+
+/// View model which exposes isRecording and error messages to observers.
+class VideoCaptureViewModel: ObservableObject {
+    private let service: VideoCaptureService = VideoCaptureService()
+    
+    @Published var isRecording = false
+    @Published var error: Error?
+    
+    func startRecording(to url: URL) {
+        // TO-DO: implement URL passing in service
+        service.startRecording()
+        
+        // TO-DO: read the property from the service
+        isRecording = true
+    }
+    
+    func stopRecording() {
+        service.stopRecording()
+        
+        isRecording = false
+    }
+}
