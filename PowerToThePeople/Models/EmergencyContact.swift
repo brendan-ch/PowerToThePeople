5a7c1ee1c7fd96eddeca19650ce78943f31c2084
Brendan Chen <me@bchen.dev>
2024-01-20 13:58:53 -0800

    add PhoneNumberKit + EmergencyContact data model

---
+++ PowerToThePeople/Models/EmergencyContact.swift
@@ -0,0 +1,23 @@
+//
+//  EmergencyContact.swift
+//  PowerToThePeople
+//
+//  Created by Brendan Chen on 2024.01.20.
+//
+
+import Foundation
+import SwiftData
+import PhoneNumberKit
+
+@Model
+final class EmergencyContact {
+    let name: String
+    let phoneNumber: PhoneNumber
+    let message: String
+    
+    init(name: String, phoneNumber: PhoneNumber, message: Stsring) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.message = message
+    }
+}
