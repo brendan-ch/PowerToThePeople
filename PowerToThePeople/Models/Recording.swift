6a9dff37cba57bb35d4189f834a169d62af840fc
Brendan Chen <me@bchen.dev>
2024-01-19 22:51:03 -0800

    add Recording model

---
+++ PowerToThePeople/Models/Recording.swift
@@ -0,0 +1,40 @@
+//
+//  Recording.swift
+//  PowerToThePeople
+//
+//  Created by Brendan Chen on 2024.01.19.
+//
+
+import Foundation
+import SwiftData
+import CoreLocation
+
+@Model
+final class Recording {
+    /// Location of the video recording from the back camera (facing away from the user).
+    let backCameraFile: URL
+    
+    /// Location of the video recording from the front camera.
+    let frontCameraFile: URL
+    
+    /// The timestamp that the recording was started.
+    let timestampStarted: Date
+    
+    /// The timestamp that the recording was ended.
+    let timestampEnded: Date
+    
+    /// The location that the recording was taken.
+    let location: CLLocation?
+    
+    /// Any notes added by the user.
+    let notes: String
+    
+    init(backCameraFile: URL, frontCameraFile: URL, timestampStarted: Date, timestampEnded: Date, location: CLLocation?, notes: String) {
+        self.backCameraFile = backCameraFile
+        self.frontCameraFile = frontCameraFile
+        self.timestampStarted = timestampStarted
+        self.timestampEnded = timestampEnded
+        self.location = location
+        self.notes = notes
+    }
+}
