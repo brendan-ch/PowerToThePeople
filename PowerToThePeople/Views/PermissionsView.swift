e568a9690ab784c9319158f6523d39c3660b6f09
Brendan Chen <me@bchen.dev>
2024-01-20 21:04:58 -0800

    add permissions setup screen

---
+++ PowerToThePeople/Views/PermissionsView.swift
@@ -0,0 +1,86 @@
+//
+//  PermissionsView.swift
+//  PowerToThePeople
+//
+//  Created by Brendan Chen on 2024.01.20.
+//
+
+import Foundation
+import SwiftUI
+
+/// View for setting up user permissions.
+struct PermissionsView: View {
+    var body: some View {
+        VStack(spacing: 8) {
+            Text("Permissions Setup")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Power to the People needs the following permissions to work.")
+                .multilineTextAlignment(.center)
+            
+            VStack(spacing: 8) {
+                Button {} label: {
+                    Text("Request camera access")
+                        .fontWeight(.bold)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                Text("We use the front and back camera to record interactions with police.")
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.vertical)
+            
+            VStack(spacing: 8) {
+                Button {} label: {
+                    Text("Request microphone access")
+                        .fontWeight(.bold)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                Text("We use the microphone to record interactions with police.")
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.vertical)
+            
+            VStack(spacing: 8) {
+                Button {} label: {
+                    Text("Request location access (optional)")
+                        .fontWeight(.bold)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                Text("If enabled, your videos will have location data associated with them.")
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.vertical)
+
+            Button {} label: {
+                Text("Continue")
+                    .fontWeight(.bold)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    PermissionsView()
+}
